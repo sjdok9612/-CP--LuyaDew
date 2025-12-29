cd2d9c73db9270d0901abae428e1fd7acd428b82
sjdok9612 <sjdok9612@gmail.com>
2025-12-29 21:30:58 +0900

    1229 : submarine interact

---
+++ assets/Maps/p.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="p" tilewidth="16" tileheight="16" tilecount="1312" columns="41">
+ <image source="SubmarineInside.png" width="656" height="512"/>
+ <tile id="257">
+  <animation>
+   <frame tileid="257" duration="400"/>
+   <frame tileid="258" duration="400"/>
+   <frame tileid="339" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="298">
+  <animation>
+   <frame tileid="298" duration="400"/>
+   <frame tileid="299" duration="400"/>
+   <frame tileid="380" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/Maps/p2.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="p2" tilewidth="16" tileheight="16" tilecount="140" columns="7">
+ <image source="submarineOutside-Sheet-sheet-sheet.png" width="112" height="320"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="35" duration="400"/>
+   <frame tileid="70" duration="400"/>
+   <frame tileid="105" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
